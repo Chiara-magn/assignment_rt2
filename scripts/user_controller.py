3e3e3e01c52978ae4997d7b1b7310e766ea1d692
Chiara-magn <chiara.magnasco03@gmail.com>
2026-01-01 09:46:03 +0000

    added initial user control file, added msgs file added initial servicies

--- scripts/user_controller.py
+++ scripts/user_controller.py
@@ -1,3 +1,4 @@
+# Initial user controller node
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
@@ -12,28 +13,36 @@ class UserController(Node):
     def __init__(self):
         super().__init__('user_controller')
 
+        # cmd/vel publisher
         self.cmd_pub = self.create_publisher(Twist, '/cmd_vel', 10)
 
+        # Qbstacle info publisher
         self.obs_pub = self.create_publisher(ObstacleInfo, '/obstacle_info', 10)
 
+        # Laser subscriber
         self.create_subscription(LaserScan, '/scan', self.laser_callback, 10)
 
+        # Servicies client
         self.get_avg_client = self.create_client(GetAverages, 'get_averages')
         self.set_thr_client = self.create_client(SetThreshold, 'set_threshold')
 
+        # Initial threshold (has to be modified)
         self.threshold = 0.5
 
 
     def laser_callback(self, scan):
+        # Laser minimum frontal distance 
         front_ranges = scan.ranges[len(scan.ranges)//3 : 2*len(scan.ranges)//3]
         min_front = min(front_ranges)
 
+        # Obstacle info publication
         info = ObstacleInfo()
         info.min_distance = float(min_front)
         info.threshold = float(self.threshold)
         info.is_obstacle = min_front < self.threshold
         self.obs_pub.publish(info)
 
+        # Movement based on threshold (has to be modified)
         if min_front < self.threshold:
             stop = Twist()
             self.cmd_pub.publish(stop)
